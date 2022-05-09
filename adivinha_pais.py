69bba24fe72eebbf54919e075390ef91b1485975
FernandoMattos16 <fernandopinheiromattos@gmail.com>
2022-05-09 17:05:22 -0300

    mais 3 funções

--- adivinha_pais.py
+++ adivinha_pais.py
@@ -1,4 +1,6 @@
 import random
+import math
+
 
 def adiciona_em_ordem(pais, distancia, lista):
     nova_lista = []
@@ -34,3 +36,34 @@ def sorteia_letra(palavra, lista):
         if i not in lista:
             nova_palavra += i
     return random.choice(nova_palavra)
+
+
+def normaliza(dic_paises):
+    dic_normalizado = {}
+    dic_valor = {}
+    for continente,paises in dic_paises.items():
+        for pais,espec in paises.items():
+            dic_valor = espec
+            dic_valor['continente'] = continente
+            dic_normalizado[pais] = dic_valor
+    return dic_normalizado
+
+
+def sorteia_pais(dicionario):
+    lista = []
+    for nome_pais in dicionario.keys():
+        lista.append(nome_pais)
+    pais_sorteado = random.choice(lista)
+    return pais_sorteado
+
+
+def haversine(raio,fi1,lambida1,fi2,lambida2):
+    fi1r = math.radians(fi1)
+    fi2r = math.radians(fi2)
+    lambida1r = math.radians(lambida1)
+    lambida2r = math.radians(lambida2)
+    a = (math.sin((fi2r-fi1r)/2))**2
+    b = math.cos(fi1r)*math.cos(fi2r)
+    c = (math.sin((lambida2r-lambida1r)/2))**2
+    distancia = 2*raio * math.asin((a + b*c)**0.5)
+    return distancia
