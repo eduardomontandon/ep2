b5471253a8d39faf4cf0529157ade59520f2baa1
FernandoMattos16 <fernandopinheiromattos@gmail.com>
2022-05-10 20:39:52 -0300

    começo do jogo

---
+++ jogo_v1.py
@@ -0,0 +1,28 @@
+from funções import *
+from dados import DADOS, EARTH_RADIUS
+
+
+print(' ============================')
+print('|                             |')
+print('| Bem vindo ao Insper Países  |')
+print('|                             |')
+print(' ==== Design de Software ====\n')
+    
+print('Comandos: \n')
+print('          dica     - entra no mercado de dicas')
+print('          desisto  - desiste da rodada\n')
+
+print('Um país foi escolhido, tente adivinhar!')
+print('Você tem 20 tentativas!\n')
+
+palpite = str(input('Qual seu palpite? ')).lower().strip()
+
+#FUNÇÕES
+dic_normalizado = normaliza(DADOS)
+escolhido = sorteia_pais(dic_normalizado)
+
+if palpite == escolhido:
+    print('Muito bem!')
+
+if palpite != escolhido:
+    print('O país escolhido era {}'.format(escolhido))
